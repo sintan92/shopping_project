867a9c27064913382800593707a0a308dc586d5b
Sinan Özbek <sinan_ozbek@hotmail.com>
2020-11-12 13:10:12 +0100

    first commit

---
+++ src/test/java/com/shopping/ShoppingProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
